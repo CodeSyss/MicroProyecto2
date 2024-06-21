38b6e27778770883981fa4ddb923dae1c022a77e
Reinaldo Reyes <reinaldo.reyes@correo.unimet.edu.ve>
2024-06-21 13:46:09 -0400

    Css en proceso

---
+++ src/Components/LoginForm.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './LoginForm.css';
+import  {FaUser} from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
+const LoginForm = () => {
+    return (
+        <div className="wrapper">
+            <form action="">
+                
+                <h1> <IoIosArrowBack className="icon"/> <FaUser className="icon"/> Iniciar Sesión</h1>
+                <div className="input-box">
+
+                    <input type="text" placeholder="Email" required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Contraseña" required/>
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Recordarme</label>
+                </div>
+    
+                <button type="submit">Acceder</button>
+
+                <div className="register_link">
+                    <p>No tienes cuenta?  <a href="#">Registrate</a></p>
+                </div>
+            </form>
+        
+        </div>
+    );
+    }
+export default LoginForm;
