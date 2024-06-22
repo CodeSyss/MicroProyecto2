555a85438ffa5079011d4b57e23abd7353506458
Reinaldo Reyes <reinaldo.reyes@correo.unimet.edu.ve>
2024-06-22 16:58:29 -0400

    Cambio a js

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h5>Prueba</h5>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
