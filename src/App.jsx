6cf71a5f74d148260b0a230672c0cbfed1ace9ea
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-06-24 03:29:03 -0400

    adicion del input rol + perfil (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import HeaderUser from './Components/HeaderUser'
+import FotoPerfil from './Components/FotoPerfil'
+import DatosUsuario from './Components/DatosUsuario'
 import './App.css'
 
 function App() {
   return (
     <>
       <HeaderUser />
-
+      <FotoPerfil />
+      <DatosUsuario />
     </>
   )
 }
